1c0eb0aec15727c73ab5c7881fe051eecddabff6
Wilmer Arenas <wilmerarenas24@gmail.com>
2025-01-14 20:36:28 -0500

    Listar clientes

--- src/main/java/zona_fit/datos/ClinteDao.java
+++ src/main/java/zona_fit/datos/ClinteDao.java
@@ -1,13 +1,49 @@
 package zona_fit.datos;
 
+import zona_fit.conexion.Conexion;
 import zona_fit.dominio.Cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import static zona_fit.conexion.Conexion.getConexion;
 
 public class ClinteDao implements IClienteDao{
     @Override
     public List<Cliente> listarClientes() {
-        return List.of();
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps; //Permite preparar sentenca sql
+        ResultSet rs;//Recibe informacion de consulta sql
+        Connection con = getConexion();
+        var sql =  "SELECT * FROM cliente ORDER BY id";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+
+        }catch (Exception e){
+            System.out.println("Error en la consulta clientes: "+e.getMessage());
+
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar conexion: "+e.getMessage());
+            }
+        }
+
+        return clientes;
     }
 
     @Override
